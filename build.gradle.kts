de31d6cceb73486e35de6df11f7d9ced31bf271f
savagavran <gavransava@gmail.com>
2025-02-04 22:55:59 +0100

    Project configuration

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+
+        // This is used only for internal Google builds.
+        maven { url = uri("../nowinandroid-prebuilts/m2repository") }
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.room) apply false
+}
